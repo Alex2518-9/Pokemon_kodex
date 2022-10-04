fcb7d8cd2e7915e56495bb50af516b88ab2debe7
Teringer Alex <ateringer@b2international.com>
2022-10-04 17:35:53 +0200

    feat(fetchingData): not using this endPoints

--- pages/api/getAllPokes.ts
+++ pages/api/getAllPokes.ts
@@ -1,17 +1,24 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
+export interface Pok {
+   name: string,
+   url?: string
+}
 
-async function getAllPokemon(req:NextApiRequest, res: NextApiResponse) {
+async function getAllPokemon(req: NextApiRequest, res: NextApiResponse) {
    try {
-      const allData = await axios.get("https://pokeapi.co/api/v2/pokemon/");
-      console.log(allData.data)
-      
-   
-      
-      res.status(200).send(allData);
+      const allData: Pok = await axios.get("https://pokeapi.co/api/v2/pokemon/")
+         .then((res) => res.data.results.map((item: Pok) => {
+            console.log(item);
+
+            return item
+         }));
+      console.log(allData);
+
+      res.status(200).json(allData);
    } catch (error) {
-      res.status(404).send({error})
+      res.status(404).send({ error })
    }
 }
 
