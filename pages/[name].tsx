de970d0f8913a23d110c23cfeddedc5d555277b5
Teringer Alex <ateringer@b2international.com>
2022-10-04 17:53:18 +0200

    feat(staticProps): try to use

--- pages/[name].tsx
+++ pages/[name].tsx
@@ -1,5 +1,25 @@
 import React from 'react'
 import styles from "../styles/PokemonDetail.module.css"
+import useFetchPokemon from '../utils/useRequest';
+import { Pok } from './api/getAllPokes';
+
+
+export async function getStaticProps({ name, url }: Pok) {
+  const postData = await useFetchPokemon(name);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = useFetchPokemon();
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 const pokemonDetail = () => {
   return (
